3e16783429e63262ad05190ffdcebe5aeeb14816
Bruno Oliveira <bruno@faber.vc>
2016-01-23 14:37:20 +0000

    mysql_real_escape_string to mysqli_real_escape_string(

--- GameEngine/Protection.php
+++ GameEngine/Protection.php
@@ -15,15 +15,15 @@ global $database;
 
 if(isset($_POST)){
 	if(!isset($_POST['ft'])){
-		$_POST = $database->sanitate($_POST)
+		$_POST = $database->sanitate($_POST);
 		$_POST = array_map('htmlspecialchars', $_POST);
 	}
 }
 
 $rsargs=$_GET['rsargs'];
-$_GET = $database->sanitate($_GET)
+$_GET = $database->sanitate($_GET);
 $_GET = array_map('htmlspecialchars', $_GET);
 $_GET['rsargs']=$rsargs;
-$_COOKIE = $database->sanitate($_COOKIE)
+$_COOKIE = $database->sanitate($_COOKIE);
 $_COOKIE = array_map('htmlspecialchars', $_COOKIE);
 ?>
