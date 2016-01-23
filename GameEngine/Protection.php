815c64fa7eab8f222b768e7ca5508454bce634ad
Bruno Oliveira <bruno@faber.vc>
2016-01-23 14:14:42 +0000

    mysql_real_escape_string to mysqli_real_escape_string(

--- GameEngine/Protection.php
+++ GameEngine/Protection.php
@@ -13,14 +13,14 @@
 //heef npc uitzondering omdat die met speciaal $_post werken
 if(isset($_POST)){
 	if(!isset($_POST['ft'])){
-	$_POST = @array_map('$database->escape_string_query', $_POST);
+	$_POST = @array_map('mysql_real_escape_string', $_POST);
 	$_POST = array_map('htmlspecialchars', $_POST);
 	}
 }
 			$rsargs=$_GET['rsargs'];
-$_GET = array_map('$database->escape_string_query', $_GET);
+$_GET = array_map('mysql_real_escape_string', $_GET);
 $_GET = array_map('htmlspecialchars', $_GET);
 $_GET['rsargs']=$rsargs;
-$_COOKIE = array_map('$database->escape_string_query', $_COOKIE);
+$_COOKIE = array_map('mysql_real_escape_string', $_COOKIE);
 $_COOKIE = array_map('htmlspecialchars', $_COOKIE);
 ?>
